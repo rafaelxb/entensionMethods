43808c2ff3df04810774bf9de9677e35b139d5b6
rafaelxb <rfxbarbosa@gmail.com>
2023-03-25 18:36:54 -0300

    Adicionar arquivos de projeto.

---
+++ entensionMethods/Program.cs
@@ -0,0 +1,9 @@
+﻿DateTime dt1 = new DateTime(2023, 03, 22, 8, 10, 45);
+DateTime dt2 = new DateTime(2023, 03, 25, 8, 10, 45);
+
+Console.WriteLine(dt1.ElapsedTime());
+Console.WriteLine(dt2.ElapsedTime());
+
+string text = "Good morning dear students!";
+
+Console.WriteLine(text.Cut(12));
